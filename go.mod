edaa9db5dd5cea9756bfdd80035a3bce8fc922f8
oumed <mouedrassi@gmail.com>
2021-10-14 18:30:43 -0400

    Ref3

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/oumed/packagem
 
 go 1.16
+
+require github.com/oumed/packagea v0.0.0-20211014193335-1b5265643968
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/oumed/packagea"
+)
 
 func main(){
-	fmt.Println("Hi every one ...")
+	//fmt.Println("Hi every one ...")
+	aa := packagea.GetName("HHHHHHH")
+
+	fmt.Printf(aa)
 }
